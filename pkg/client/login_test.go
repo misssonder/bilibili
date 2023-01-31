89c12d8d8b5bf41e94db954b6b5f9d68afe6071a
misssonder <819643718@qq.com>
2023-01-31 19:22:13 +0800

    update

--- pkg/client/login_test.go
+++ pkg/client/login_test.go
@@ -1,24 +1,36 @@
 package client
 
 import (
-	"testing"
-
 	"github.com/misssonder/bilibili/internal/util"
+	"github.com/misssonder/bilibili/pkg/qrcode"
+	"os"
+	"testing"
 )
 
 func TestLogin(t *testing.T) {
 	client := &Client{}
-	qrcode, err := client.GenerateQrcode()
+	qrcodeResp, err := client.GenerateQrcode()
 	if err != nil {
 		t.Error(err)
 		return
 	}
-	t.Log(util.MustMarshal(qrcode))
+	t.Log(util.MustMarshal(qrcodeResp))
 
-	pollQrcode, err := client.PollQrcode(qrcode.Data.QrcodeKey)
-	if err != nil {
-		t.Error(err)
+	if err = qrcode.Generate(qrcodeResp.Data.URL, qrcode.Low, os.Stdout); err != nil {
 		return
 	}
-	t.Log(util.MustMarshal(pollQrcode))
+	for {
+		pollQrcode, err := client.PollQrcode(qrcodeResp.Data.QrcodeKey)
+		if err != nil {
+			t.Error(err)
+			return
+		}
+		if pollQrcode.Data.Code != 0 {
+			continue
+		} else {
+			t.Log(util.MustMarshal(pollQrcode))
+			break
+		}
+	}
+
 }
