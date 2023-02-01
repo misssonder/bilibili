52fc759a74ebe42e39f8dae6de96a7296a65c149
misssonder <819643718@qq.com>
2023-02-01 19:42:57 +0800

    add play url test

--- pkg/client/video_test.go
+++ pkg/client/video_test.go
@@ -1,6 +1,7 @@
 package client
 
 import (
+	"os"
 	"testing"
 
 	"github.com/misssonder/bilibili/internal/util"
@@ -14,4 +15,31 @@ func TestClient_GetVideoInfo(t *testing.T) {
 		return
 	}
 	t.Log(util.MustMarshalIndent(info))
+}
+
+func TestClient_PlayUrl(t *testing.T) {
+	client := &Client{}
+	responses, err := client.LoginWithQrCode(os.Stdout)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	for resp := range responses {
+		switch resp.LoginStatus {
+		case LoginSuccess:
+			client.SetCookie(resp.Cookie)
+			break
+		case LoginExpired:
+			t.Log(LoginExpired)
+			return
+		default:
+			continue
+		}
+	}
+	resp, err := client.PlayUrl("https://www.bilibili.com/video/BV1XG4y1X7HV/?spm_id_from=333.1007.tianma.1-3-3.click&vd_source=76326787bdfce30577382b0e7e18f35c", "973082699", 120)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	t.Log(util.MustMarshalIndent(resp))
 }
